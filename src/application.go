f96fae3014f5d4e69afb12f1243969ef1bebc33a
xiexiangpeng <xie_xiangpeng@wuxiapptec.com>
2018-12-08 22:05:28 +0800

    redefine api

--- src/application.go
+++ src/application.go
@@ -16,21 +16,29 @@ func main() {
 	//修改配置文件内容
 	router.POST("/config/:fileId", nil)
 
-	//更新镜像
-	router.POST("/image", nil)
+	//获取镜像列表
+	router.GET("/image", nil)
+	//拉取镜像pull image
+	router.GET("/image/:repo/:tag", nil)
+	//启动容器docker run
+	router.POST("/image/:repo/:tag", nil)
+	//删除镜像rmi
+	router.DELETE("/image/:repo/:tag", nil)
 	//获取容器列表
 	router.GET("/container", handler.Containers)
 	//查看容器具体信息
 	router.GET("/container/:containerId", nil)
-	//启动容器
+	//停止容器stop
 	router.POST("/container/:containerId", nil)
-	//停止容器
+	//删除容器rm
 	router.DELETE("/container/:containerId", handler.StopContainer)
 
 	//通过docker-compose up启动服务
 	router.POST("/compose/:fileId", handler.ComposeHandler)
 	//通过docker-compose pull更新镜像
 	router.GET("/compose/:fileId", handler.ComposeHandler)
+	//通过docker-compose down停止服务
+	router.DELETE("/compose/:fileId", handler.ComposeHandler)
 
 	http.ListenAndServe(":8080", router)
 }
